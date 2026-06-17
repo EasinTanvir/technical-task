1507c79c1e52e2b3d6d97f5b6fa32aa1f0b487ca
EasinTanvir <easintanvir102@gmail.com>
2026-06-18 01:15:10 +0600

    added membership card

--- src/components/landingPage/MemberShip/MemberShip.jsx
+++ src/components/landingPage/MemberShip/MemberShip.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Container from "../../shared/Container";
 
+import TierCard from "./TierCard";
+import FreeForever from "./FreeForever";
+
 const MemberShip = () => {
   return (
     <section className="py-16 md:py-24">
-      <Container>MemberShip</Container>
+      <Container>
+        <TierCard />
+        <FreeForever />
+      </Container>
     </section>
   );
 };
---
+++ src/components/landingPage/MemberShip/TierCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TierCard = () => {
+  return <div></div>;
+};
+
+export default TierCard;
