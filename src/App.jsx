d8507a5e5c3752f087f174539458e4b7b73ad792
EasinTanvir <easintanvir102@gmail.com>
2026-06-18 22:39:46 +0600

    remove lazy loader from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { lazy } from "react";
+import React from "react";
 
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/landingPage/Hero/HeroSection";
@@ -6,15 +6,9 @@ import RootLayout from "./components/layouts/RootLayout";
 import GlobalReach from "./components/landingPage/GlobalReach/GlobalReach";
 import MemberShip from "./components/landingPage/MemberShip/MemberShip";
 import CustomProfile from "./components/landingPage/CustomProfile/CustomProfile";
+import AreYouReady from "./components/landingPage/AreYouReady/AreYouReady";
 import CommonQuestion from "./components/landingPage/CommonQuestion/CommonQuestion";
-
-const AreYouReady = lazy(
-  () => import("./components/landingPage/AreYouReady/AreYouReady"),
-);
-
-const HelpClick = lazy(
-  () => import("./components/landingPage/HelpClick/HelpClick"),
-);
+import HelpClick from "./components/landingPage/HelpClick/HelpClick";
 
 const App = () => {
   return (
