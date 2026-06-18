1bfbfb36c242862a648a4da03b2c6bd9ae34cd9b
EasinTanvir <easintanvir102@gmail.com>
2026-06-18 19:39:47 +0600

    added transition to nav button

--- src/components/Navbar/NavItems.jsx
+++ src/components/Navbar/NavItems.jsx
@@ -4,11 +4,33 @@ import Button from "../ui/Button";
 const NavItems = () => {
   return (
     <div className="flex items-center gap-4">
-      <Button className="text-heading-light text-sm font-semibold">
+      <Button
+        className="
+    text-heading-light
+    text-sm
+    font-semibold
+    transition-opacity
+    duration-300
+    hover:opacity-80
+  "
+      >
         Sign In
       </Button>
 
-      <Button className="bg-primary-button text-heading-light text-sm font-semibold rounded-2xl py-2 px-2.5">
+      <Button
+        className="
+    bg-primary-button
+    text-heading-light
+    text-sm
+    font-semibold
+    rounded-2xl
+    py-2
+    px-2.5
+    transition-all
+    duration-300
+    hover:scale-[1.02]
+  "
+      >
         Sign Up
       </Button>
     </div>
