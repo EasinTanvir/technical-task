082a104c6f096b8a80ab001aa68ca7774cb75f32
EasinTanvir <easintanvir102@gmail.com>
2026-06-18 23:36:29 +0600

    update container inline padding

--- src/components/shared/Container.jsx
+++ src/components/shared/Container.jsx
@@ -1,6 +1,8 @@
 const Container = ({ children, className = "" }) => {
   return (
-    <div className={`mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ${className}`}>
+    <div
+      className={`mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 xl:px-14 ${className}`}
+    >
       {children}
     </div>
   );
